7623b664991b65066e85fef582463aa1543c30fc
User <asylzatwww@yandex.ru>
2019-03-14 16:00:25 +0300

    db

--- config/db.php
+++ config/db.php
@@ -2,15 +2,15 @@
 
 return [
     'class' => 'yii\db\Connection',
-/*    'dsn' => 'mysql:host=localhost;dbname=x2agam8n_glossar',
+    'dsn' => 'mysql:host=localhost;dbname=x2agam8n_glossar',
     'username' => 'x2agam8n_glossar',
     'password' => 'zOoRncGf',
-  */
 
+/*
     'dsn' => 'mysql:host=http://s25.webhost1.ru;dbname=yaformat_kf',
     'username' => 'yaformat_kf',
     'password' => 's8^X~c',
-    'charset' => 'utf8',
+  */  'charset' => 'utf8',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
