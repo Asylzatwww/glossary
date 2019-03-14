2343cd161f269ea12fe0d9f659e1f9dae8abaa53
/usr/bin/vi <support@beget.ru>
2019-03-14 15:02:40 +0300

    error of hell

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=x2agam8n_glossar',
+    'username' => 'x2agam8n_glossar',
+    'password' => 'zOoRncGf',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
