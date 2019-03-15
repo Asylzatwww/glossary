d15c6c523813a10f863325954ede0614dc88ec65
User <asylzatwww@yandex.ru>
2019-03-15 10:08:39 +0300

    image load

--- controllers/GlossaryController.php
+++ controllers/GlossaryController.php
@@ -9,6 +9,7 @@ use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 use app\models\UploadForm;
+use yii\web\UploadedFile;
 
 /**
  * GlossaryController implements the CRUD actions for Glossary model.
